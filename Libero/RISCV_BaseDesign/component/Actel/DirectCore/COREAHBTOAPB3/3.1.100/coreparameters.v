c497a5e9a9509268ad5d44297cca96de936830d2
Cyril Jean <cyril.s.jean@gmail.com>
2016-07-31 21:43:45 +0100

    Initial Libero design

---
+++ Libero/RISCV_BaseDesign/component/Actel/DirectCore/COREAHBTOAPB3/3.1.100/coreparameters.v
@@ -0,0 +1,9 @@
+//--------------------------------------------------------------------
+// Created by Microsemi SmartDesign Sat Jul 30 17:46:41 2016
+// Parameters for COREAHBTOAPB3
+//--------------------------------------------------------------------
+
+
+parameter FAMILY = 19;
+parameter HDL_license = "U";
+parameter testbench = "User";
---
+++ Libero/RISCV_BaseDesign/component/Actel/DirectCore/CoreTimer/2.0.103/coreparameters.v
@@ -0,0 +1,9 @@
+//--------------------------------------------------------------------
+// Created by Microsemi SmartDesign Sat Jul 30 17:46:42 2016
+// Parameters for CoreTimer
+//--------------------------------------------------------------------
+
+
+parameter FAMILY = 19;
+parameter INTACTIVEH = 1;
+parameter WIDTH = 32;
