83254ce18421b9047d52c970515605292bcaafc9
CLappin <ciaran.lappin@microsemi.com>
2017-07-03 16:38:51 +0100

    Removed CoreRISCV_BaseDesign

--- Modify_The_FPGA_Design/CoreRISCV_AXI4_BaseDesign/component/Actel/DirectCore/CORERISCV_AXI4/2.0.102/coreparameters.v
+++
@@ -1,8 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Mon Jun 12 11:05:32 2017
-// Parameters for CORERISCV_AXI4
-//--------------------------------------------------------------------
-
-
-parameter FAMILY = 19;
-parameter RESET_VECTOR_ADDR = 'h60000000;
